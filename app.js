e5062b1b37a6f93513b481915687cc2eb875f41f
danivanee <danivanee_170@hotmail.com>
2023-03-03 22:33:36 -0600

    se agrego condicional a app.js

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
+
+navToggle.addEventListener('click', function() {
+    //console.log(links.classList);
+    if(links.classList.contains('show-links')){
+        links.classList.remove('show-links')
+    }
+});
