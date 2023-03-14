d935bc033c27b083ff9e59b745ba982fe479787d
danivanee <danivanee_170@hotmail.com>
2023-03-13 21:51:56 -0600

    se concluyo proyecto

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const links = document.querySelector(".links");
 
 navToggle.addEventListener('click', function() {
     //console.log(links.classList);
-    if(links.classList.contains('show-links')){
+    /*if(links.classList.contains('show-links')){
         links.classList.remove('show-links')
-    }
+    } */
+    links.classList.toggle("show-links")
 });
+
